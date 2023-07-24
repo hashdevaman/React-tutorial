72d8be6f4a69058bdea71a2bd947d48d48623bb5
Aman Kumar <aman.kumar@hashstudioz.com>
2023-07-24 12:37:52 +0530

    React stop watch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+// Initial state of timer
+const initialState = {
+  isRunning: false,
+  timer: 0
+};
+
+// Adding reducer function
+const reducer = (state, action) => {
+  // console.log("action here", action);
+
+  switch (action.type) {
+    case "start":
+      return { ...state, isRunning: true };
+    case "stop":
+      return { ...state, isRunning: false };
+    case "reset":
+      return { isRunning: false, timer: 0 };
+    case "tick":
+      return { ...state, timer: state.timer + 1 };
+    default:
+      throw new Error();
+  }
+};
+
+export default function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const intervalRef = React.useRef(0);
+
+  React.useEffect(() => {
+    if (!state.isRunning) {
+      return;
+    }
+    intervalRef.current = setInterval(() => dispatch({ type: "tick" }), 1000);
+    return () => {
+      clearInterval(intervalRef.current);
+      intervalRef.current = 0;
+    };
+  }, [state.isRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Stop Watch</h2>
+      <h3>{state.timer}s</h3>
+      <button onClick={() => dispatch({ type: "start" })}>Start</button>
+      <button onClick={() => dispatch({ type: "stop" })}>Stop</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 }
-
-export default App;
